c736b28d052310f91e590bbed2483a7ea51fa171
François <francois.monniot@smartthings.com>
2021-03-26 10:56:29 -0700

    Add code taken from the epd 4.2 example

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ The variable `MQTT_C_CLIENT_TRACE_MAX_LINES` limits the number of lines of trace
 export MQTT_C_CLIENT_TRACE=ON
 export MQTT_C_CLIENT_TRACE_LEVEL=PROTOCOL
 ```
+
+
+# Cross compiling
+
+_Note: This is for compiling to the raspberry pi. Depending on your device, the target may be different._
+
+
+We use [cross](https://github.com/rust-embedded/cross/pull/522) to simplify the setup.
+
+1. rustup toolchain add stable-x86_64-unknown-linux-gnu --profile=minimal
+2. cargo install cross
+3. cross build --target=armv7-unknown-linux-gnueabihf
+
+The binary will be found in `target/armv7-unknown-linux-gnueabihf/debug/mqtt-console`.
+
+Note: When doing a release, pass the `--release` flag and look in the `release` directory instead of `debug`.
+
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,148 @@ use std::time::SystemTime;
 use paho_mqtt as mqtt;
 use futures::stream::StreamExt;
 
+use embedded_graphics::{
+    fonts::{Font12x16, Font6x8, Text},
+    prelude::*,
+    primitives::{Circle, Line},
+    style::PrimitiveStyle,
+    text_style,
+};
+use embedded_hal::prelude::*;
+use epd_waveshare::{
+    color::*,
+    epd7in5_v2::{Display7in5, EPD7in5},
+    graphics::{Display, DisplayRotation},
+    prelude::*,
+};
+use linux_embedded_hal::{
+    spidev::{self, SpidevOptions},
+    sysfs_gpio::Direction,
+    Delay, Pin, Spidev,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+    // Configure SPI
+    // Settings are taken from
+    let mut spi = Spidev::open("/dev/spidev0.0").expect("spidev directory");
+    let options = SpidevOptions::new()
+        .bits_per_word(8)
+        .max_speed_hz(4_000_000)
+        .mode(spidev::SpiModeFlags::SPI_MODE_0)
+        .build();
+    spi.configure(&options).expect("spi configuration");
+
+    // Configure Digital I/O Pin to be used as Chip Select for SPI
+    let cs = Pin::new(26); //BCM7 CE0
+    cs.export().expect("cs export");
+    while !cs.is_exported() {}
+    cs.set_direction(Direction::Out).expect("CS Direction");
+    cs.set_value(1).expect("CS Value set to 1");
+
+    let busy = Pin::new(5); //pin 29
+    busy.export().expect("busy export");
+    while !busy.is_exported() {}
+    busy.set_direction(Direction::In).expect("busy Direction");
+    //busy.set_value(1).expect("busy Value set to 1");
+
+    let dc = Pin::new(6); //pin 31 //bcm6
+    dc.export().expect("dc export");
+    while !dc.is_exported() {}
+    dc.set_direction(Direction::Out).expect("dc Direction");
+    dc.set_value(1).expect("dc Value set to 1");
+
+    let rst = Pin::new(16); //pin 36 //bcm16
+    rst.export().expect("rst export");
+    while !rst.is_exported() {}
+    rst.set_direction(Direction::Out).expect("rst Direction");
+    rst.set_value(1).expect("rst Value set to 1");
+
+    let mut delay = Delay {};
+
+    // Display7in5, EPD7in5
+    let mut epd7in5 =
+        EPD7in5::new(&mut spi, cs, busy, dc, rst, &mut delay).expect("eink initalize error");
+
+    println!("Test all the rotations");
+    let mut display = Display7in5::default();
+
+    display.set_rotation(DisplayRotation::Rotate0);
+    draw_text(&mut display, "Rotate 0!", 5, 50);
+
+    display.set_rotation(DisplayRotation::Rotate90);
+    draw_text(&mut display, "Rotate 90!", 5, 50);
+
+    display.set_rotation(DisplayRotation::Rotate180);
+    draw_text(&mut display, "Rotate 180!", 5, 50);
+
+    display.set_rotation(DisplayRotation::Rotate270);
+    draw_text(&mut display, "Rotate 270!", 5, 50);
+
+    epd7in5.update_frame(&mut spi, &display.buffer())?;
+    epd7in5
+        .display_frame(&mut spi)
+        .expect("display frame new graphics");
+    delay.delay_ms(5000u16);
+
+    println!("Now test new graphics with default rotation and some special stuff:");
+    display.clear_buffer(Color::White);
+
+    // draw a analog clock
+    let _ = Circle::new(Point::new(64, 64), 64)
+        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+        .draw(&mut display);
+    let _ = Line::new(Point::new(64, 64), Point::new(0, 64))
+        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+        .draw(&mut display);
+    let _ = Line::new(Point::new(64, 64), Point::new(80, 80))
+        .into_styled(PrimitiveStyle::with_stroke(Black, 1))
+        .draw(&mut display);
+
+    // draw white on black background
+    let _ = Text::new("It's working-WoB!", Point::new(175, 250))
+        .into_styled(text_style!(
+            font = Font6x8,
+            text_color = White,
+            background_color = Black
+        ))
+        .draw(&mut display);
+
+    // use bigger/different font
+    let _ = Text::new("It's working-WoB!", Point::new(50, 200))
+        .into_styled(text_style!(
+            font = Font12x16,
+            text_color = White,
+            background_color = Black
+        ))
+        .draw(&mut display);
+
+    // a moving `Hello World!`
+    let limit = 10;
+    // Can set on the 7"5 because it does not support partial refresh
+    //epd7in5.set_lut(&mut spi, Some(RefreshLUT::QUICK)).unwrap();
+    epd7in5.clear_frame(&mut spi).unwrap();
+    for i in 0..limit {
+        println!("Moving Hello World. Loop {} from {}", (i + 1), limit);
+
+        draw_text(&mut display, "  Hello World! ", 5 + i * 12, 50);
+
+        epd7in5.update_frame(&mut spi, &display.buffer()).unwrap();
+        epd7in5
+            .display_frame(&mut spi)
+            .expect("display frame new graphics");
+
+        delay.delay_ms(1_000u16);
+    }
+
+    println!("Finished tests - going to sleep");
+    epd7in5.sleep(&mut spi);
+
+
+    return Ok(());
+
     // TODO Change the .with_file_name to not have to pass a dummy file name here
     let cfg = Configuration::from_directory("/Users/francoismonniot/Projects/local/mqtt-console/data/project/nothing")?;
 
@@ -68,7 +207,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         format!("/v1/notifications/{}", &cfg.stcli.device_id),
     ];
     let topics_qos = [1, 1];
-    mqtt_client.subscribe_many(&topics, &topics_qos).await?;
+    //mqtt_client.subscribe_many(&topics, &topics_qos).await?;
 
     println!("subscribed");
 
@@ -91,9 +230,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //let msg = mqtt::Message::new(topic, "{}", mqtt::QOS_1);
     //mqtt_client.publish(msg).await?;
 
-    sleep(Duration::from_millis(3000)).await;
+    //sleep(Duration::from_millis(30_000)).await;
+    println!("Sleep expired, closing");
+
+    mqtt_client.disconnect(None).await?;
 
     Ok(())
+}
+
+
+fn draw_text(display: &mut Display7in5, text: &str, x: i32, y: i32) {
+    let _ = Text::new(text, Point::new(x, y))
+        .into_styled(text_style!(
+            font = Font6x8,
+            text_color = Black,
+            background_color = White
+        ))
+        .draw(display);
 }
 
 // JWT (custom implementation for now. TODO see if we can use a standard crate)
