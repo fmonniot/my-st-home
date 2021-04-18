ed2d5cf671f1879d9da51043c003c0b0219b3969
François <francois.monniot@smartthings.com>
2021-04-18 14:21:49 -0700

    new design for UDP actors
    Let's try it for LIFX and see if it does improve
    the LIFX section of the code.

--- src/actor/network.rs
+++ src/actor/network.rs
@@ -7,14 +7,10 @@
 
 pub mod udp {
     use crate::actor::{Actor, ActorRef, Context, Message, Receiver, StreamHandle};
-    use futures::{SinkExt, stream::{SplitSink, SplitStream, StreamExt}};
     use std::convert::From;
     use std::net::SocketAddr;
     use tokio::net::UdpSocket;
-    use tokio_util::{
-        codec::{Decoder, Encoder},
-        udp::UdpFramed,
-    };
+    use tokio_util::codec::{Decoder, Encoder};
 
     pub enum CreationError {
         IO(std::io::Error),
@@ -34,16 +30,16 @@ pub mod udp {
 
     impl<I: Message> Message for Msg<I> {}
 
-    pub struct Udp<C, I>
+    pub struct Udp<C>
     where
         C: Decoder,
     {
         addr: SocketAddr,
-        sink: SplitSink<UdpFramed<C>, (I, SocketAddr)>,
-        // This is only used to pass the stream between the struct creation
+        write: WriteFramedUdp<C>,
+        // This field is only used to pass the stream between the struct creation
         // and the actor starting. At which point the stream will be taken
         // and used to feed messages to the actor itself.
-        stream: Option<SplitStream<UdpFramed<C>>>,
+        read: Option<ReadFramedUdp<C>>,
 
         stream_handle: Option<StreamHandle>,
 
@@ -54,9 +50,9 @@ pub mod udp {
         addr: SocketAddr,
         codec: C,
         target: ActorRef<A>,
-    ) -> Result<Udp<C, I>, CreationError>
+    ) -> Result<Udp<C>, CreationError>
     where
-        C: Decoder<Item = I> + Encoder<I> + Unpin,
+        C: Decoder<Item = I> + Encoder<I> + Unpin + Clone,
         I: Message,
         A: Receiver<UdpPacketResult<C::Item, <C as Decoder>::Error>>,
         <C as Decoder>::Error: Message,
@@ -65,9 +61,12 @@ pub mod udp {
         // which me don't really do in this project (we use IP addresses only in UDP)
         let handle = tokio::runtime::Handle::current();
         let socket = handle.block_on(UdpSocket::bind(addr.clone()))?;
-        let framed = UdpFramed::new(socket, codec);
 
-        let (sink, stream) = framed.split();
+        // TODO Let's not do that. Instead, let's have our own framed struct which take an
+        // Arc<UdpSocket> and do only the read part (impl Stream only).
+        // We can then store the socket in the actor state and handle write directly instead
+        // of going through the cumbersome Sink interface
+        let (write, read) = framed(socket, codec);
 
         let send_packet = move |item| {
             target.send_msg(item);
@@ -75,47 +74,46 @@ pub mod udp {
 
         Ok(Udp {
             addr,
-            sink,
-            stream: Some(stream),
+            write,
+            read: Some(read),
             stream_handle: None,
             send_packet: Box::new(send_packet),
         })
     }
 
-    impl<C, I> Actor for Udp<C, I>
+    impl<C> Actor for Udp<C>
     where
-        I: Message,
-        C: Decoder<Item = I> + Send + 'static + Unpin,
+        C: Decoder + Send + 'static + Unpin,
         C::Item: Message,
         C::Error: Message,
     {
         fn pre_start(&mut self, ctx: &Context<Self>) {
-            if let Some(stream) = self.stream.take() {
+            if let Some(stream) = self.read.take() {
                 let handle = ctx.subscribe_to_stream(stream);
                 self.stream_handle = Some(handle);
             }
         }
     }
 
-    impl<C, I> Receiver<Msg<I>> for Udp<C, I>
+    impl<C, I> Receiver<Msg<I>> for Udp<C>
     where
         I: Message,
-        C: Decoder<Item = I> + Encoder<I> + Send + Unpin + 'static,
+        C: Decoder<Item = I> + Encoder<I> + Send + Unpin + Clone + 'static,
         <C as Decoder>::Item: Message,
         <C as Decoder>::Error: Message,
         <C as Encoder<I>>::Error: Send,
-     {
+    {
         fn recv(&mut self, _ctx: &Context<Self>, msg: Msg<I>) {
             match msg {
                 Msg::Write(item) => {
                     let addr = self.addr.clone();
-                    let s = self.sink.send((item, addr));
+                    let mut w = self.write.clone();
+
                     tokio::spawn(async move {
-                        s.await
+                        let _ = w.send((item, addr)).await; // TODO Error handling
                     });
                 }
             }
-
         }
     }
 
@@ -128,7 +126,7 @@ pub mod udp {
     {
     }
 
-    impl<C, I> Receiver<UdpPacketResult<C::Item, C::Error>> for Udp<C, I>
+    impl<C, I> Receiver<UdpPacketResult<C::Item, C::Error>> for Udp<C>
     where
         I: Message,
         C: Decoder<Item = I> + Send + Unpin + 'static,
@@ -137,6 +135,130 @@ pub mod udp {
     {
         fn recv(&mut self, _ctx: &Context<Self>, msg: UdpPacketResult<C::Item, C::Error>) {
             (self.send_packet)(msg)
+        }
+    }
+
+    // Framing construct for UDP packets. Trying to get something based off
+    // tokio-util for reading, but with easier writing capacity.
+
+    use bytes::BufMut;
+    use bytes::BytesMut;
+    use core::task::{self, Poll};
+    use futures::Stream;
+    use futures_core::ready;
+    use std::mem::MaybeUninit;
+    use std::pin::Pin;
+    use std::sync::Arc;
+    use tokio::io::ReadBuf;
+
+    /// A [`Stream`] interface to an underlying `UdpSocket`, using
+    /// the `Decoder` traits to decode frames.
+    ///
+    /// Taken from `tokio-util` with the `Sink` trait removed. This is because
+    /// the `Sink` trait is too bothersome to use in the context of an actor.
+    /// Instead we provides a simpler [`WriteFramedUdp`].
+    ///
+    /// See [`tokio_util::udp::UdpFramed`] for original `Stream` implementation.
+    struct ReadFramedUdp<C> {
+        socket: Arc<UdpSocket>,
+        codec: C,
+        rd: BytesMut, // rename to buf
+        is_readable: bool,
+        current_addr: Option<SocketAddr>,
+    }
+
+    #[derive(Clone)]
+    struct WriteFramedUdp<C> {
+        socket: Arc<UdpSocket>,
+        codec: C,
+        buf: BytesMut,
+    }
+
+    // TODO Can we go with lower buffers ? Evaluate the average size frame for LIFX.
+    const INITIAL_RD_CAPACITY: usize = 64 * 1024;
+    const INITIAL_WR_CAPACITY: usize = 8 * 1024;
+
+    fn framed<C>(socket: UdpSocket, codec: C) -> (WriteFramedUdp<C>, ReadFramedUdp<C>)
+    where
+        C: Clone,
+    {
+        let socket = Arc::new(socket);
+
+        let write = WriteFramedUdp {
+            socket: socket.clone(),
+            codec: codec.clone(),
+            buf: BytesMut::with_capacity(INITIAL_WR_CAPACITY),
+        };
+
+        let read = ReadFramedUdp {
+            socket,
+            codec,
+            rd: BytesMut::with_capacity(INITIAL_RD_CAPACITY),
+            is_readable: false,
+            current_addr: None,
+        };
+
+        (write, read)
+    }
+
+    impl<C> WriteFramedUdp<C> {
+        async fn send<I>(self: &mut Self, item: (I, SocketAddr)) -> Result<(), C::Error>
+        where
+            C: Encoder<I>,
+        {
+            let (frame, out_addr) = item;
+
+            self.codec.encode(frame, &mut self.buf)?;
+            self.socket.send_to(&self.buf, out_addr).await?;
+            self.buf.clear();
+
+            Ok(())
+        }
+    }
+
+    // See tokio_util::udp::UdpFramed for original implementation (version 0.6.3)
+    impl<C: Decoder + Unpin> Stream for ReadFramedUdp<C> {
+        type Item = Result<(C::Item, SocketAddr), C::Error>;
+
+        fn poll_next(self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Option<Self::Item>> {
+            let pin = self.get_mut();
+
+            pin.rd.reserve(INITIAL_RD_CAPACITY);
+
+            loop {
+                // Are there still bytes left in the read buffer to decode?
+                if pin.is_readable {
+                    if let Some(frame) = pin.codec.decode_eof(&mut pin.rd)? {
+                        let current_addr = pin
+                            .current_addr
+                            .expect("will always be set before this line is called");
+
+                        return Poll::Ready(Some(Ok((frame, current_addr))));
+                    }
+
+                    // if this line has been reached then decode has returned `None`.
+                    pin.is_readable = false;
+                    pin.rd.clear();
+                }
+
+                // We're out of data. Try and fetch more data to decode
+                let addr = unsafe {
+                    // Convert `&mut [MaybeUnit<u8>]` to `&mut [u8]` because we will be
+                    // writing to it via `poll_recv_from` and therefore initializing the memory.
+                    let buf = &mut *(pin.rd.chunk_mut() as *mut _ as *mut [MaybeUninit<u8>]);
+                    let mut read = ReadBuf::uninit(buf);
+                    let ptr = read.filled().as_ptr();
+                    let res = ready!(Pin::new(&mut pin.socket).poll_recv_from(cx, &mut read));
+
+                    assert_eq!(ptr, read.filled().as_ptr());
+                    let addr = res?;
+                    pin.rd.advance_mut(read.filled().len());
+                    addr
+                };
+
+                pin.current_addr = Some(addr);
+                pin.is_readable = true;
+            }
         }
     }
 }
