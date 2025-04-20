9abebafb85f6e2582bbb9d172860e1eae1fdb615
François Monniot <francoismonniot@gmail.com>
2025-04-20 14:36:39 -0700

    need to own the domain explicitly

--- my-home/src/smartthings/mqtt.rs
+++ my-home/src/smartthings/mqtt.rs
@@ -456,10 +456,10 @@ mod runloop {
 
         // Why can't that be done in one step ?
         let s: &str = &opts.host;
-        let domain = s.try_into()?;
+        let domain: rustls::pki_types::ServerName<'_> = s.try_into()?;
 
         let tcp = TcpStream::connect((opts.host.as_str(), opts.port)).await?;
-        let tls_stream = tls_connector.connect(domain, tcp).await?;
+        let tls_stream = tls_connector.connect(domain.to_owned(), tcp).await?;
 
         let f = Framed::new(tls_stream, MqttCodec::new());
 
